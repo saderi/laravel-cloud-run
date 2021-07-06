f7ad640b6ea30914e71d531d474e1a56614c1eb6
payam <saderi@gmail.com>
2021-07-06 22:48:05 +0300

    remove DB_SOCKET

--- .docker/docker-php-entrypoint.sh
+++ .docker/docker-php-entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 nohup cloud_sql_proxy -instances=${SQL_CONNECTION_NAME}=tcp:3306 -dir=/cloudsql/ &
 
-sed -i "s/DB_SOCKET=.*/DB_SOCKET=${MYSQL_BD_SOCKET}/g" /var/www/html/.env.example
 sed -i "s/DB_DATABASE=.*/DB_DATABASE=${DB_DATABASE}/g" /var/www/html/.env.example
 sed -i "s/DB_USERNAME=.*/DB_USERNAME=${DB_USERNAME}/g" /var/www/html/.env.example
 sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=${DB_PASSWORD}/g" /var/www/html/.env.example

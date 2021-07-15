bcf9a0a76e42fdd68af961fadebae8b689900a5c
payam <saderi@gmail.com>
2021-07-15 13:59:42 +0300

    Test cloud run

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <h1>Test Cloud run</h1>
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
